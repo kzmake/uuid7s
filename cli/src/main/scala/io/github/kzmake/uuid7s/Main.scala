14aac15d4a2acabc6e12312b2184b0a9c014c9b5
kzmake <kzmake.i3a@gmail.com>
2021-12-26 22:57:23 +0900

    cli: add Main

---
+++ cli/src/main/scala/io/github/kzmake/uuid7s/Main.scala
@@ -0,0 +1,28 @@
+package io.github.kzmake.uuid7s
+
+import org.rogach.scallop._
+
+import java.time._
+
+case class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  // generate
+  val n = opt[Int](default = Some(1), validate = _ > 0)
+
+  // parse
+  val local  = opt[Boolean](default = Some(false))
+  val uuidv7 = trailArg[String](required = false)
+
+  verify()
+}
+
+object Main extends App {
+  val conf = new Conf(args.toSeq)
+  conf.uuidv7.toOption match {
+    case None => (1 to conf.n.apply()).foreach(_ => println(UUID.generate().toString))
+    case Some(v) =>
+      val uuid    = UUID.parse(v).get
+      val instant = Instant.ofEpochSecond(uuid.timestamp, uuid.msec * 1_000_000)
+      if (conf.local.apply()) println(OffsetDateTime.ofInstant(instant, ZoneId.systemDefault()))
+      else println(OffsetDateTime.ofInstant(instant, ZoneOffset.UTC))
+  }
+}
